b343790d0cfdb375a3995d277a03a3431fe57ff1
Ben Sharafian <benms64@gmail.com>
2018-01-16 12:53:21 -0800

    fix: improve script

--- scripts/get_domain.js
+++ scripts/get_domain.js
@@ -1,6 +1,11 @@
 const base32 = require('base32')
 const addressCodec = require('ripple-address-codec')
 
+if (!process.argv[2]) {
+  console.error('usage: node scripts/get_domain.js <ripple_address>')
+  process.exit(1)
+}
+
 const address = process.argv[2]
 console.log('ripple-address:  ', address)
 console.log('domain:          ',
